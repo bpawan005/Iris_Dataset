3f915cf82b49487ab8cda0db29926635c100e5f5
Pawan <bpawan005@gmail.com>
2022-10-23 02:46:06 +0530

    Iris project created

---
+++ config.py
@@ -0,0 +1,4 @@
+JSON_FILE_PATH = r"E:\Velocity classes\10. October 2022\10_21_Logistic_Multiclass\Pawan Multiclass Iris flower\Iris.json"
+CSV_FILE_PATH = r"E:\Velocity classes\10. October 2022\10_21_Logistic_Multiclass\Pawan Multiclass Iris flower\Iris.csv"
+PORT_NO = 5555
+MODEL_FILE_PATH = r"E:\Velocity classes\10. October 2022\10_21_Logistic_Multiclass\Pawan Multiclass Iris flower\Iris.pkl"
